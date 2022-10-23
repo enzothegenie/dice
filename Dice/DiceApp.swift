6c7cd709a99bd0fe25673cfdf14f36a711119c26
enzothegenie <enzothegenie@gmail.com>
2022-10-23 11:44:11 +0200

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Vincenzo Gulizzi on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
